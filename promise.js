4c4ea363b6d399082da9ba3eb95fd8d4f24d0548
naquib1229 <naquib.alam1229@gmail.com>
2022-11-21 08:04:18 +0530

    Promise catch error

--- promise.js
+++ promise.js
@@ -2,10 +2,13 @@ const p = new Promise((resolve, reject) => {
     // Kick off some async work
     //...
     setTimeout(() =>{
-        resolve(1);
+        //resolve(1);
+        reject(new Error('message'));
     },2000); 
-   
-    //reject(new Error('message'));
+    
 });
 
-p.then(result => console.log('Result', result));
+p
+    .then(result => console.log('Result', result))
+    .catch(err => console.log('Error', err.message));
+
