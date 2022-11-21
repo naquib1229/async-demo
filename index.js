07873858dc0d3d0ab5e13872247baff14ef39341
naquib1229 <naquib.alam1229@gmail.com>
2022-11-21 07:53:20 +0530

    Promises intro

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-//Callback Hell
-//Issue caused by coding with complex nested callbacks.
+//Promise
+//Holds the eventual result of an asynchronous operation
+//Pending ------->async operation          Fulfilled or rejected
 
 console.log('Before');
-getUser(1, function(user) {
-    getRepositories(user.gitHubUsername, (repos) => {
-        console.log('Repos' repos);
-    })
-});
+getUser(1, getRepositories);
+
 console.log('After');
+
+function getRepositories(user) {
+    getRepositories(user.gitHubUsername, getCommits);
+}
+function getCommits(repos){
+    getCommits(repo, displayCommits);
+}
+function displayCommits(commits){
+    console.log(commits);
+}
+
 
 function getUser(id, callback) {
    setTimeout(() => {
@@ -16,10 +25,16 @@ function getUser(id, callback) {
 },  2000);
 }
 
-function getRepositories(username, callback){
+function getRepositories(username, callback1){
     setTimeout(() => {
         console.log('Calling GitHub API.. ');
-        callback( ['repo1', 'repo2', 'repo3'] )
+        callback1(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
+function getCommits(repo, callback){
+    setTimeout(() => {
+        console.log('Fetching commits');
+        callback(['commit1', 'commit2']);
     }, 2000);
 }
 
